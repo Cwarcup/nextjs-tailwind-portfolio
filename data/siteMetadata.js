69a01af13f66f4376642c833f8532a0e8a48f57e
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-11-16 14:15:52 -0800

    initial setup

---
+++ data/siteMetadata.js
@@ -0,0 +1,7 @@
+const siteMetadata = {
+  title: 'cwarcup - Starter Template',
+  description: 'A starter template for Next.js projects.',
+  author: 'Curtis Warcup',
+}
+
+export default siteMetadata
